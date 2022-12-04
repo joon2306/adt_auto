cede4a4f9e1f9b63134ea3067a53f50c81a77a7e
Arjoon <pertauba@gmail.com>
2022-12-04 23:25:15 +0400

    Add environment

--- src/components/TokenGenerator.js
+++ src/components/TokenGenerator.js
@@ -4,11 +4,14 @@ import "./TokenGenerator.css";
 import axios from "axios";
 import notificationService from "../services/NotificationService";
 import loaderService from "../services/LoaderService";
+import Form from "react-bootstrap/Form";
+import Environment from "../lib/Environment";
 
 export default function TokenGenerator() {
   const [variantRenault, setVariantRenault] = useState("warning");
   const [variantNissan, setVariantNissan] = useState("primary");
   const [disable, setDisable] = useState(false);
+  const [val, setVal] = useState(1);
   const baseUri = "http://localhost:4000";
 
   const getToken = (nissan) => {
@@ -16,7 +19,9 @@ export default function TokenGenerator() {
     setVariantRenault("secondary");
     setVariantNissan("secondary");
     loaderService.load();
-    const uri = nissan ? baseUri + "/nissan" : baseUri;
+    let uri = nissan ? baseUri + "/nissan" : baseUri + "/renault";
+    const env = Environment[val];
+    uri = `${uri}/${env}`;
     axios
       .get(uri)
       .then((response) => {
@@ -39,12 +44,27 @@ export default function TokenGenerator() {
       });
   };
 
+  const select = (e) => {
+    const value = e.target.value;
+    setVal(value);
+  }
+
   return (
     <>
       <div>
         <h1 style={{ textAlign: "center", marginBottom: "30px" }}>
           Token Generator
         </h1>
+
+        <Form.Select aria-label="Default select example" onChange={(e) => select(e)}>
+          <option value="1">Devint</option>
+          <option value="2">Ft1</option>
+          <option value="3">Dev</option>
+          <option value="4">RT</option>
+          <option value="5">DEV-MP</option>
+          <option value="6">RT-MP</option>
+        </Form.Select>
+
         <Button
           variant={variantRenault}
           className={
---
+++ src/lib/Environment.js
@@ -0,0 +1,11 @@
+const Environment = Object.freeze({
+    1: "devint",
+    2: "ft1",
+    3: "dev",
+    4: "rt",
+    5: "dev-mp",
+    6: "rt-mp"
+
+});
+
+export default Environment;
